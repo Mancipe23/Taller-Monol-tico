5a03073618bc450f0106582d3a9b053b6d00e4fc
04davidmartinezg <ndmartinezg@uniboyaca.edu.com>
2026-04-08 18:09:27 -0500

    conexion a la base de datos

---
+++ models/config/model_base.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\models\config;
+
+abstract class ModelBase
+{
+    public function get($prop)
+    {
+        return $this->{$prop};
+    }
+
+    public function set($prop, $value)
+    {
+        $this->{$prop} = $value;
+    }
+}
