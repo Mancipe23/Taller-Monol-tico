e4d77a654c51ccb5b298645f06052f5774e41273
04davidmartinezg <ndmartinezg@uniboyaca.edu.co>
2026-04-11 07:25:13 -0500

    vehiculo entidad

--- models/entities/vehiculo.php
+++ models/entities/vehiculo.php
@@ -1,18 +1,21 @@
 <?php
 
 namespace app\models\entities;
-
 use app\models\config\ModelBase;
-
-class Estudiante extends ModelBase
-{
+class Vehiculo extends ModelBase{
+   
     protected $id = 0;
-    protected $nombre = null;
-    protected $email = null;
-
-    public function __construct($id, $nombre, $email)
-    {
+    protected $marca = null;
+    protected $modelo;
+    protected $anio;
+    protected $categoria;
+    protected $estado;
+    public function __construct($id, $marca, $modelo, $anio, $categoria, $estado = 'Disponible') { //se utiliza el funcion __construct para definir un constructor que se ejecuta automáticamente al crear una nueva instancia de la clase. Este constructor recibe parámetros para inicializar los atributos del vehículo, y el estado por defecto se establece como 'Disponible'.
         $this->id = $id;
-        $this->nombre = $nombre;
-        $this->email = $email;
-    }
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->anio = $anio;
+        $this->categoria = $categoria;
+        $this->estado = $estado;
+    } 
+ }
