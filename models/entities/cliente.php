ef876db22698d3120a94493e106bf14a7a93fc03
04davidmartinezg <ndmartinezg@uniboyaca.edu.co>
2026-04-11 07:33:01 -0500

    entidad cliente

---
+++ models/entities/cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\models\entities;
+use app\models\config\ModelBase;
+
+class Cliente extends ModelBase
+{
+    protected $id = 0;
+    protected $nombre = null;
+    protected $telefono = null;
+    protected $email = null;
+    protected $numero_licencia = 0;
+
+    public function __construct($id, $nombre, $telefono ,$email, $numero_licencia)
+    {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this -> telefono = $telefono;
+        $this->email = $email;
+        $this->numero_licencia = $numero_licencia;
+    }
+
+   
+}
